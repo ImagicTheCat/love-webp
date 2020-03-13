8c878516aa865f7c62678f74af1bd0e9ba85de7a
Imagic <imagicthecat@gmail.com>
2020-03-13 20:16:16 +0100

    Add WebP examples.
    - short anim
    - long anim
    - still overlay

--- examples/conf.lua
+++ examples/conf.lua
@@ -1,5 +1,6 @@
 
 function love.conf(t)
+  t.window.title = "Cursed WebP examples"
   t.window.width = 1280
   t.window.height = 720
 end
--- examples/main.lua
+++ examples/main.lua
@@ -1,6 +1,7 @@
 local WebP = require("love-webp")
 
 local overlay, short_anim, long_anim
+local t_overlay, t_short, t_long = true, true, true
 function love.load()
   short_anim = love.graphics.newArrayImage((WebP.loadImages(love.filesystem.newFileData("short-anim.webp"))))
 
@@ -11,16 +12,37 @@ function love.load()
 end
 
 function love.update(dt)
-  long_anim:tick(dt)
+  if t_long then
+    long_anim:tick(dt)
+  end
 end
 
 function love.draw()
-  -- display short anim centered
-  love.graphics.drawLayer(short_anim, (love.timer.getTime()*15)%short_anim:getLayerCount()+1, love.graphics.getWidth()/2-short_anim:getWidth()/2, love.graphics.getHeight()/2-short_anim:getHeight()/2)
+  if t_short then
+    -- display short anim
+    local layer = (love.timer.getTime()*15)%short_anim:getLayerCount()+1
+    for i=0,15 do
+      for j=0,8 do
+        love.graphics.drawLayer(short_anim, layer, i*80, j*80)
+      end
+    end
+  end
 
-  -- display long anim centered
-  love.graphics.draw(long_anim.texture, love.graphics.getWidth()/2-long_anim.texture:getWidth()/2, love.graphics.getHeight()/2-long_anim.texture:getHeight()/2)
+  if t_long then
+    -- display long anim centered
+    love.graphics.draw(long_anim.texture, love.graphics.getWidth()/2-long_anim.texture:getWidth()/2, love.graphics.getHeight()/2-long_anim.texture:getHeight()/2)
+  end
 
-  -- display overlay
-  love.graphics.draw(overlay)
+  if t_overlay then
+    -- display overlay
+    love.graphics.draw(overlay)
+  end
+
+  love.graphics.print("toggles (scancode):\n[Q] short anim\n[W] long anim\n[E] overlay", 0,0)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if scancode == "q" then t_short = not t_short
+  elseif scancode == "w" then t_long = not t_long
+  elseif scancode == "e" then t_overlay = not t_overlay end
 end
